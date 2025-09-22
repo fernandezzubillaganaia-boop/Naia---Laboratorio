66baf68cf9d7952fb4bbde3cbd1cb0ea813af581
fernandezzubillaganaia-boop <fernandezzubillaga.naia@gmail.com>
2025-09-22 17:03:08 +0200

    Add files via upload

---
+++ LABORATORIODIA22-09NAIA.sql
@@ -0,0 +1,8 @@
+SELECT * FROM likes;
+SELECT*FROM usuarios;
+SELECT*FROM seguidores;
+SELECT*FROM videos;
+SELECT*FROM comentarios;
+SELECT*FROM videos JOIN usuarios;
+SELECT*FROM videos JOIN comentarios;
+SELECT*FROM likes JOIN seguidores;
